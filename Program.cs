a7f963124be0aedbad1ae0dc6ebad9d9da88e6dd
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-02-25 14:10:41 -0500

    Calculated area of Rectangle

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace AreaFor4Shapes
             radius = Convert.ToDouble(Console.ReadLine());
 
             circleArea = Math.PI * radius *radius;
-
             Console.WriteLine("\nArea of a Circle with radius {0} is {1}",radius, circleArea);
 
 
@@ -31,10 +30,24 @@ namespace AreaFor4Shapes
 
 
             triangleArea = .5 * baseLength * height;
-
-
-
             Console.WriteLine("\nArea of a Triangle with baseLength of {0} and height of {1} is {2}", baseLength, height, triangleArea);
+
+
+
+            double length, width, rectangleArea;
+
+            Console.WriteLine("\nEnter the length of the rectangle");
+
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n Enter the width of the rectangle");
+
+            width = Convert.ToDouble(Console.ReadLine());
+
+
+
+            rectangleArea = length * width;
+            Console.WriteLine("\nArea of a Rectangle with length of {0} and width {1} is {2}", length, width, rectangleArea);
 
         }
     }
