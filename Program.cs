fd9b00ff583919ba16b79b4ea311ead0da2082ef
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-02-25 14:06:02 -0500

    Calculated area of Circle

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AreaFor4Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius, circleArea;
+            Console.WriteLine("Enter the radius of the circle");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            circleArea = Math.PI * radius *radius;
+
+            Console.WriteLine("\nArea of a Circle with radius {0} is {1}",radius, circleArea);
+
+        }
+    }
+}
