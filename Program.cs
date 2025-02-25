74672bd2d58fff99050d5ee71b650cd1f685d82b
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-02-25 14:08:29 -0500

    Calculated area of Triangle

--- Program.cs
+++ Program.cs
@@ -16,6 +16,26 @@ namespace AreaFor4Shapes
 
             Console.WriteLine("\nArea of a Circle with radius {0} is {1}",radius, circleArea);
 
+
+
+            double baseLength, height, triangleArea;
+
+            Console.WriteLine("\nEnter the baseLength of the triangle");
+
+            baseLength = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter the height of the triangle");
+
+            height = Convert.ToDouble(Console.ReadLine());
+
+
+
+            triangleArea = .5 * baseLength * height;
+
+
+
+            Console.WriteLine("\nArea of a Triangle with baseLength of {0} and height of {1} is {2}", baseLength, height, triangleArea);
+
         }
     }
 }
